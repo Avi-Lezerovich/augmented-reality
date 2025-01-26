535bc0678f28d59b4e37a0fb196356c96d1562c7
Avi Lezerovich <avi.lezerovich@gmail.com>
2025-01-26 12:46:42 +0200

    improved clarity

--- core/feature_utils.py
+++ core/feature_utils.py
@@ -1,42 +1,35 @@
 import cv2
 import numpy as np
 
+sift = cv2.SIFT_create()
 
-class FeatureMatcher:
-    def __init__(self):
-        self.sift = cv2.SIFT_create()
-        self.bf = cv2.BFMatcher()
-        
-    display = False
-        
-
-    def find_keypoints_and_matches(self, img1, img2):
-        gray_1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-        gray_2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-        kp_l, desc_l = self.sift.detectAndCompute(gray_1, None)
-        kp_r, desc_r = self.sift.detectAndCompute(gray_2, None)
-        
-       
-
-        if self.display:
-            test = cv2.drawKeypoints(img1, kp_l, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-            cv2.imshow('keypoint', test)
-            cv2.waitKey(0)
-            
-        matches = self.bf.knnMatch(desc_l, desc_r, k=2)
-        
-        if self.display:
-            img3 = cv2.drawMatchesKnn(img1, kp_l, img2, kp_r, matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-            cv2.imshow('matches', img3)
-            cv2.waitKey(0)
-            
-        return kp_l, kp_r, matches
-        
+def find_keypoints(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return sift.detectAndCompute(gray, None)
     
-    def find_homography(self, kp1, kp2, matches):
-        good_kp1 = np.array([kp1[m.queryIdx].pt for m in matches])
-        good_kp2 = np.array([kp2[m.trainIdx].pt for m in matches])
-        H, masked = cv2.findHomography(good_kp2, good_kp1, cv2.RANSAC, 5.0)
-        
-        return H, masked
+
+def find_matches(des1, des2, display=False):
+     # FLANN parameters and matcher
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
     
+    matches = flann.knnMatch(des1, des2, k=2)
+
+    good_matches = []
+    for m, n in matches:
+        if m.distance < 0.7 * n.distance: # Apply ratio test
+            good_matches.append(m)
+
+    return good_matches
+
+def find_homography(kp1, kp2, matches):
+    if kp1 is None or kp2 is None or len(matches) < 4: 
+        return None, None
+    
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+    return cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+
